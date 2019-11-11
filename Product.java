6a834d81fa40bf56f60b6ef8b58b44e4fb531902
Gerrydh <35727450+Gerrydh@users.noreply.github.com>
2019-11-11 16:33:09 +0000

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,37 @@
+package ShopVideoVersion;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author gerardh
+ */
+public class Product {
+    private String name;
+    private double price;
+
+    public Product(String name, double price){
+        this.name = name;
+        this.price = price;
+    }    
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "name=" + name + ", price=" + price + '}'; //uses square brackets in video
+    }
+    
+    
+    
+}
